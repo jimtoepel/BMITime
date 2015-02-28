f7a85a33c95ff207af572eba76472f4c3b3a715b
jimtoepel <jim.toepel@gmail.com>
2015-02-27 19:22:34 -0800

    Worked by dateString is deprecated!

--- BMITime/main.m
+++ BMITime/main.m
@@ -13,20 +13,24 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
 
         // Create and instance of BNREmployee
-        BNRPerson *mikey = [[BNREmployee alloc] init];
+        BNREmployee *mikey = [[BNREmployee alloc] init];
         
         // Give that instance interesting values using "Setters"
         mikey.weightInKilos = 96;
         mikey.heightInMeters = 1.8;
+        mikey.employeeID = 12;
+        mikey.hireDate = [NSDate dateWithNaturalLanguageString:@"Aug 2nd, 2010"];
         
         // Log the instance varriables using the "getters"
         float height = mikey.heightInMeters;
         int weight = mikey.weightInKilos;
         NSLog(@"mikey is %.2f meters tall and weighs %d kilograms", height, weight);
+        NSLog(@"Employee %u hired on %@", mikey.employeeID, mikey.hireDate);
         
         // Log some values using custom methods
         float bmi = [mikey bodyMassIndex];
-        NSLog(@"mikey has a BMI of %f", bmi);
+        double years = [mikey yearsOfEmployment];
+        NSLog(@"BMI of %.2f, has worked with us for %.2f years", bmi, years);
         
         NSLog(@"Hello, World!");
     }
